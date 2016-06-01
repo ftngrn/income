c0ce2629ff868d3b7c4529af8e3d37ce16b5d412
[SG] Noriaki Ishihara <noriaki.ishihara@sapporo-gakuen.jp>
2016-06-01 14:27:08 +0900

    Add thumbnail action

--- src/Controller/PhotosController.php
+++ src/Controller/PhotosController.php
@@ -11,98 +11,75 @@ use App\Controller\AppController;
 class PhotosController extends AppController
 {
 
-    /**
-     * Index method
-     *
-     * @return \Cake\Network\Response|null
-     */
-    public function index()
-    {
-        $photos = $this->paginate($this->Photos);
+	/**
+	 * Initialize
+	 *
+	 */
+	public function initialize()
+	{
+		parent::initialize();
+		$this->loadComponent('RequestHandler');
+	}
 
-        $this->set(compact('photos'));
-        $this->set('_serialize', ['photos']);
-    }
+	/**
+	 * Thumbnail method
+	 *
+	 * @param string|null $id Photo id.
+	 * @return \Cake\Network\Response|null
+	 * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+	 */
+	public function thumbnail($id = null)
+	{
+		$this->autoRender = false;
+		$photo = $this->Photos->get($id, [
+			'contain' => []
+		]);
+		$cropped = $this->crop(stream_get_contents($photo->body));
+		$this->response->type($photo->mime);
+		$this->response->length(strlen($cropped));
+		$this->response->body($cropped);
+	}
 
-    /**
-     * View method
-     *
-     * @param string|null $id Photo id.
-     * @return \Cake\Network\Response|null
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $photo = $this->Photos->get($id, [
-            'contain' => []
-        ]);
+	private function crop($body) {
+		$cache_days = 15; //キャッシュする日数
+		$thumb_pct = 0.1;	//元画像に対するサムネイルの比率
+		$center_pct = 0.45; //左右中央切り出しの中央率
+		$middle_pct = 0.7;	//上下中央切り出しの中央率
+		$cropped = null;
 
-        $this->set('photo', $photo);
-        $this->set('_serialize', ['photo']);
-    }
+		$im = imagecreatefromstring($body);
+		if ($im !== false) {
+			$size = getimagesizefromstring($body);
+			$w = $size[0];
+			$h = $size[1];
+			$tw = intval($w * $thumb_pct);
+			$th = intval($h * $thumb_pct);
+			$nw = intval($tw * $center_pct);
+			$nh = intval($th * $middle_pct);
+			$nw_offset = intval(abs($tw - $nw) / 2);
+			$nh_offset = intval(abs($th - $nh) / 4);
 
-    /**
-     * Add method
-     *
-     * @return \Cake\Network\Response|void Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $photo = $this->Photos->newEntity();
-        if ($this->request->is('post')) {
-            $photo = $this->Photos->patchEntity($photo, $this->request->data);
-            if ($this->Photos->save($photo)) {
-                $this->Flash->success(__('The photo has been saved.'));
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error(__('The photo could not be saved. Please, try again.'));
-            }
-        }
-        $this->set(compact('photo'));
-        $this->set('_serialize', ['photo']);
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Photo id.
-     * @return \Cake\Network\Response|void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $photo = $this->Photos->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $photo = $this->Photos->patchEntity($photo, $this->request->data);
-            if ($this->Photos->save($photo)) {
-                $this->Flash->success(__('The photo has been saved.'));
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error(__('The photo could not be saved. Please, try again.'));
-            }
-        }
-        $this->set(compact('photo'));
-        $this->set('_serialize', ['photo']);
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Photo id.
-     * @return \Cake\Network\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $photo = $this->Photos->get($id);
-        if ($this->Photos->delete($photo)) {
-            $this->Flash->success(__('The photo has been deleted.'));
-        } else {
-            $this->Flash->error(__('The photo could not be deleted. Please, try again.'));
-        }
-        return $this->redirect(['action' => 'index']);
-    }
+			$imt = imagecreatetruecolor($tw, $th);
+			$imn = imagecreatetruecolor($nw, $nh);
+			imagecopyresampled($imt, $im, 0, 0, 0, 0, $tw, $th, $w, $h);
+			imagecopy($imn, $imt, 0, 0, $nw_offset, $nh_offset*1, $nw, $nh + $nh_offset*3);
+			//$img_dataに変換した画像を格納するため標準出力から受け取る
+			ob_start();
+			imagejpeg($imn, null, 100);
+			$cropped = ob_get_contents();
+			ob_end_clean();
+			imagedestroy($im);
+			imagedestroy($imt);
+			imagedestroy($imn);
+/*
+			//静的ファイルがなければ作成、あってもしばらくたっていれば更新
+			$cache_path = sprintf(WWW_ROOT."files".DS."%d_%s.jpg", $id, $this->action);
+			$file = new File($cache_path);
+			if ($file->exists() === false || (time() - $file->lastChange()) > 60*15) {
+				file_put_contents($cache_path, $cropped);
+			}
+*/
+		}
+		return $cropped;
+	}
 }

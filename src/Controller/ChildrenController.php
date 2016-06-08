39371c260cf94de9d8a010c01ce0973202bc5c30
[SG] Noriaki Ishihara <noriaki.ishihara@sapporo-gakuen.jp>
2016-06-09 08:28:25 +0900

    Replace 2 splaces to 1 tab.

--- src/Controller/ChildrenController.php
+++ src/Controller/ChildrenController.php
@@ -11,103 +11,104 @@ use App\Controller\AppController;
 class ChildrenController extends AppController
 {
 
-    /**
-     * Index method
-     *
-     * @return \Cake\Network\Response|null
-     */
-    public function index()
-    {
-        $this->paginate = [
-            'contain' => ['Guardians']
-        ];
-        $children = $this->paginate($this->Children);
+	/**
+	 * Index method
+	 *
+	 * @return \Cake\Network\Response|null
+	 */
+	public function index()
+	{
+		$this->paginate = [
+			'contain' => ['Guardians'],
+			'order' => ['joined' => 'desc'],
+		];
+		$children = $this->paginate($this->Children);
 
-        $this->set(compact('children'));
-        $this->set('_serialize', ['children']);
-    }
+		$this->set(compact('children'));
+		$this->set('_serialize', ['children']);
+	}
 
-    /**
-     * View method
-     *
-     * @param string|null $id Child id.
-     * @return \Cake\Network\Response|null
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $child = $this->Children->get($id, [
-            'contain' => ['Guardians', 'ChildHealths', 'ChildMedications', 'Incomes', 'Ptas']
-        ]);
+	/**
+	 * View method
+	 *
+	 * @param string|null $id Child id.
+	 * @return \Cake\Network\Response|null
+	 * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+	 */
+	public function view($id = null)
+	{
+		$child = $this->Children->get($id, [
+			'contain' => ['Guardians', 'ChildHealths', 'ChildMedications', 'Incomes', 'Ptas']
+		]);
 
-        $this->set('child', $child);
-        $this->set('_serialize', ['child']);
-    }
+		$this->set('child', $child);
+		$this->set('_serialize', ['child']);
+	}
 
-    /**
-     * Add method
-     *
-     * @return \Cake\Network\Response|void Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $child = $this->Children->newEntity();
-        if ($this->request->is('post')) {
-            $child = $this->Children->patchEntity($child, $this->request->data);
-            if ($this->Children->save($child)) {
-                $this->Flash->success(__('The child has been saved.'));
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error(__('The child could not be saved. Please, try again.'));
-            }
-        }
-        $guardians = $this->Children->Guardians->find('list', ['limit' => 200]);
-        $this->set(compact('child', 'guardians'));
-        $this->set('_serialize', ['child']);
-    }
+	/**
+	 * Add method
+	 *
+	 * @return \Cake\Network\Response|void Redirects on successful add, renders view otherwise.
+	 */
+	public function add()
+	{
+		$child = $this->Children->newEntity();
+		if ($this->request->is('post')) {
+			$child = $this->Children->patchEntity($child, $this->request->data);
+			if ($this->Children->save($child)) {
+				$this->Flash->success(__('The child has been saved.'));
+				return $this->redirect(['action' => 'index']);
+			} else {
+				$this->Flash->error(__('The child could not be saved. Please, try again.'));
+			}
+		}
+		$guardians = $this->Children->Guardians->find('list', ['limit' => 300, 'order' => 'id DESC']);
+		$this->set(compact('child', 'guardians'));
+		$this->set('_serialize', ['child']);
+	}
 
-    /**
-     * Edit method
-     *
-     * @param string|null $id Child id.
-     * @return \Cake\Network\Response|void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $child = $this->Children->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $child = $this->Children->patchEntity($child, $this->request->data);
-            if ($this->Children->save($child)) {
-                $this->Flash->success(__('The child has been saved.'));
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error(__('The child could not be saved. Please, try again.'));
-            }
-        }
-        $guardians = $this->Children->Guardians->find('list', ['limit' => 200]);
-        $this->set(compact('child', 'guardians'));
-        $this->set('_serialize', ['child']);
-    }
+	/**
+	 * Edit method
+	 *
+	 * @param string|null $id Child id.
+	 * @return \Cake\Network\Response|void Redirects on successful edit, renders view otherwise.
+	 * @throws \Cake\Network\Exception\NotFoundException When record not found.
+	 */
+	public function edit($id = null)
+	{
+		$child = $this->Children->get($id, [
+			'contain' => []
+		]);
+		if ($this->request->is(['patch', 'post', 'put'])) {
+			$child = $this->Children->patchEntity($child, $this->request->data);
+			if ($this->Children->save($child)) {
+				$this->Flash->success(__('The child has been saved.'));
+				return $this->redirect(['action' => 'index']);
+			} else {
+				$this->Flash->error(__('The child could not be saved. Please, try again.'));
+			}
+		}
+		$guardians = $this->Children->Guardians->find('list', ['limit' => 200]);
+		$this->set(compact('child', 'guardians'));
+		$this->set('_serialize', ['child']);
+	}
 
-    /**
-     * Delete method
-     *
-     * @param string|null $id Child id.
-     * @return \Cake\Network\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $child = $this->Children->get($id);
-        if ($this->Children->delete($child)) {
-            $this->Flash->success(__('The child has been deleted.'));
-        } else {
-            $this->Flash->error(__('The child could not be deleted. Please, try again.'));
-        }
-        return $this->redirect(['action' => 'index']);
-    }
+	/**
+	 * Delete method
+	 *
+	 * @param string|null $id Child id.
+	 * @return \Cake\Network\Response|null Redirects to index.
+	 * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+	 */
+	public function delete($id = null)
+	{
+		$this->request->allowMethod(['post', 'delete']);
+		$child = $this->Children->get($id);
+		if ($this->Children->delete($child)) {
+			$this->Flash->success(__('The child has been deleted.'));
+		} else {
+			$this->Flash->error(__('The child could not be deleted. Please, try again.'));
+		}
+		return $this->redirect(['action' => 'index']);
+	}
 }

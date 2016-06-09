d1215e88470201799d071ea2ba5ecf1763a35d07
[SG] Noriaki Ishihara <noriaki.ishihara@sapporo-gakuen.jp>
2016-06-09 13:47:51 +0900

    Photo beforeMarshal for set associated-model-name

--- src/Model/Table/PhotosTable.php
+++ src/Model/Table/PhotosTable.php
@@ -2,6 +2,8 @@
 namespace App\Model\Table;
 
 use App\Model\Entity\Photo;
+use ArrayObject;
+use Cake\Event\Event;
 use Cake\ORM\Query;
 use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
@@ -15,77 +17,93 @@ use Cake\Validation\Validator;
 class PhotosTable extends Table
 {
 
-    /**
-     * Initialize method
-     *
-     * @param array $config The configuration for the Table.
-     * @return void
-     */
-    public function initialize(array $config)
-    {
-        parent::initialize($config);
+	/**
+	 * Initialize method
+	 *
+	 * @param array $config The configuration for the Table.
+	 * @return void
+	 */
+	public function initialize(array $config)
+	{
+		parent::initialize($config);
 
-        $this->table('photos');
-        $this->displayField('name');
-        $this->primaryKey('id');
+		$this->table('photos');
+		$this->displayField('name');
+		$this->primaryKey('id');
 
-        $this->addBehavior('Timestamp');
-    }
+		$this->addBehavior('Timestamp');
+	}
 
-    /**
-     * Default validation rules.
-     *
-     * @param \Cake\Validation\Validator $validator Validator instance.
-     * @return \Cake\Validation\Validator
-     */
-    public function validationDefault(Validator $validator)
-    {
-        $validator
-            ->integer('id')
-            ->allowEmpty('id', 'create');
+	/**
+	 * Default validation rules.
+	 *
+	 * @param \Cake\Validation\Validator $validator Validator instance.
+	 * @return \Cake\Validation\Validator
+	 */
+	public function validationDefault(Validator $validator)
+	{
+		$validator
+			->integer('id')
+			->allowEmpty('id', 'create');
 
-        $validator
-            ->requirePresence('model', 'create')
-            ->notEmpty('model');
+		$validator
+			->requirePresence('model', 'create')
+			->notEmpty('model');
 
-        $validator
-            ->integer('seq')
-            ->allowEmpty('seq');
+		$validator
+			->integer('seq')
+			->allowEmpty('seq');
 
-        $validator
-            ->allowEmpty('body');
+		$validator
+			->allowEmpty('body');
 
-        $validator
-            ->integer('size')
-            ->requirePresence('size', 'create')
-            ->notEmpty('size');
+		$validator
+			->integer('size')
+			->requirePresence('size', 'create')
+			->notEmpty('size');
 
-        $validator
-            ->allowEmpty('mime');
+		$validator
+			->allowEmpty('mime');
 
-        $validator
-            ->allowEmpty('name');
+		$validator
+			->allowEmpty('name');
 
-        return $validator;
-    }
+		return $validator;
+	}
 
-    /**
-     * Returns a rules checker object that will be used for validating
-     * application integrity.
-     *
-     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
-     * @return \Cake\ORM\RulesChecker
-     */
-    public function buildRules(RulesChecker $rules)
-    {
-        return $rules;
-    }
+	/**
+	 * Returns a rules checker object that will be used for validating
+	 * application integrity.
+	 *
+	 * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+	 * @return \Cake\ORM\RulesChecker
+	 */
+	public function buildRules(RulesChecker $rules)
+	{
+		return $rules;
+	}
 
-		// WithoutBody finder
-		public function findWithoutBody(Query $query, array $options) {
-			//body以外を取得する
-			return $query->select(['id', 'model_id', 'seq', 'size', 'mime', 'name', 'modified', 'created'])
-				->order(['seq' => 'desc']);
+	// WithoutBody finder
+	public function findWithoutBody(Query $query, array $options) {
+		//body以外を取得する
+		return $query->select(['id', 'model_id', 'seq', 'size', 'mime', 'name', 'modified', 'created'])
+			->order(['seq' => 'desc']);
+	}
+
+	public function beforeMarshal(Event $event, ArrayObject $data, ArrayObject $options)
+	{
+		$source = $options['association']->source();
+		if ($source) {
+			$data['model'] = $source->table();
 		}
+		if ($data['file']['error'] === UPLOAD_ERR_OK) {
+			$data['body'] = file_get_contents($data['file']['tmp_name']);
+			$data['size'] = $data['file']['size'];
+			$data['mime'] = $data['file']['type'];
+			$data['name'] = $data['file']['name'];
+		} else {
+			unset($data);
+		}
+	}
 
 }
